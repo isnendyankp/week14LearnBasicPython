9a94c8184726152028597cc009767a4b8a411b43
isnendyankp <isnendyankp@gmail.com>
2024-02-13 05:55:26 +0700

    Add type checking and update variable value in variable.py

--- variable.py
+++ variable.py
@@ -2,5 +2,12 @@
 kucing = "cat"
 
 print(kucing)
-# type() untuk mengetahui tipe data dari variabel
+# type() untuk mengetahui tipe data dari variabel dan hasilnya adalah str
+print(type(kucing))
+
+# membuat variabel dengan nama kucing dan beri nilai 4
+kucing = 4
+
+print(kucing)
+# type() untuk mengetahui tipe data dari variabel dan hasilnya adalah int
 print(type(kucing))
