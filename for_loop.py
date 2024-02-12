c6b9ab8f3ca1255da7b28e17edd1c1f4c38c3b3d
isnendyankp <isnendyankp@gmail.com>
2024-02-13 06:36:06 +0700

    Add for loop to print only "A" from array

--- for_loop.py
+++ for_loop.py
@@ -16,3 +16,8 @@ for data in array:
 array = ["A", "B", "C", "D", "E"]
 [print(data) for data in array]
 
+# for loop with array for this case we want to print only "A"
+array = ["A", "B", "C", "A"]
+array_a = [data for data in array if data == "A"]
+print(array_a)
+
