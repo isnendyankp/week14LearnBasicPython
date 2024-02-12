5f67194f4543dcc09184bc893a87543c2d2d4d22
isnendyankp <isnendyankp@gmail.com>
2024-02-13 06:00:00 +0700

    Add variable declarations with different data types

--- variable.py
+++ variable.py
@@ -11,3 +11,19 @@ kucing = 4
 print(kucing)
 # type() untuk mengetahui tipe data dari variabel dan hasilnya adalah int
 print(type(kucing))
+
+# membuat variabel dengan beberapa tipe data
+
+# string
+animal = "wolf"
+# integer
+animal_total = 3
+# float
+currency = 1.5
+# boolean
+boolean = True
+
+print(f"animal: {type(animal)}")
+print(f"animal_total: {type(animal_total)}")
+print(f"currency: {type(currency)}")
+print(f"boolean: {type(boolean)}")
