56d6002bdb77e2e2b7971b99f86c00c6d18be377
isnendyankp <isnendyankp@gmail.com>
2024-02-13 06:21:18 +0700

    cr8 inline if else

--- if_else.py
+++ if_else.py
@@ -1,13 +1,17 @@
 animal = "🐱"
 
-# basic if else
-# wolf
-if animal == "🐺":
-    gender = "jantan"
-    print("wolf")
-# cat
-elif animal == "🐱":
-    gender = "betina"
-    print("cat")
-else:
-    print("animal not found")
+# # basic if else
+# # wolf
+# if animal == "🐺":
+#     gender = "jantan"
+#     print("wolf")
+# # cat
+# elif animal == "🐱":
+#     gender = "betina"
+#     print("cat")
+# else:
+#     print("animal not found")
+
+
+# inline if else
+print("wolf") if animal == "🐺" else print("cat") if animal == "🐱" else print("animal not found")
