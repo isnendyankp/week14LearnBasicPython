1e4a52b4ae485904ceff4330fe6efd496a10fbfb
isnendyankp <isnendyankp@gmail.com>
2024-02-14 05:44:13 +0700

    create class base

---
+++ class.py
@@ -0,0 +1,15 @@
+class Car:
+    # initialize with attributes
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    
+    # method to display car information
+    def display_info(self):
+        return f"this car is a {self.brand} {self.model}"
+    
+# Create an instance of the Car class
+    my_car = Car("Toyota", "Corolla")
+
+# Call the method display_info
+print(my_car.display_info())
