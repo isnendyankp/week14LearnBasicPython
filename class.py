e9e283f109e290aa8bd2a28cac53839de598e490
isnendyankp <isnendyankp@gmail.com>
2024-02-14 05:59:50 +0700

    Refactor class methods and fix method names

--- class.py
+++ class.py
@@ -9,23 +9,17 @@ class Car:
         return f"this car is a {self.brand} {self.model}"
 
 # Create an instance of the Car class
-my_car = Car("Toyota", "Corolla")
-
-# Call the method display_info
-print(my_car.display_info())
-
-# class pokemon
 class Pokemon:
     def __init__(self, name, type, rarity="Legendary"):
         self.name = name
         self.type = type
         self.rarity = rarity
 
-    def get pokemon_info(self):
-        return(f"name: {self.type} | Type: {self.type} |Rarity: {self.rarity}")
+    def get_pokemon_info(self):
+        print (f"name: {self.name} | Type: {self.type} | Rarity: {self.rarity}")
 
 # Create an instance of the Pokemon class
 my_pokemon = Pokemon("Pikachu", "Rare")
 
 # Call the method pokemon_info
-print(my_pokemon.pokemon_info())
+print(my_pokemon.get_pokemon_info())
