f526e6e7f621e308cece27f89bb15eae80804528
isnendyankp <isnendyankp@gmail.com>
2024-02-14 05:56:35 +0700

    cr8 base class Pokemon

--- class.py
+++ class.py
@@ -13,3 +13,19 @@ my_car = Car("Toyota", "Corolla")
 
 # Call the method display_info
 print(my_car.display_info())
+
+# class pokemon
+class Pokemon:
+    def __init__(self, name, type, rarity="Legendary"):
+        self.name = name
+        self.type = type
+        self.rarity = rarity
+
+    def get pokemon_info(self):
+        return(f"name: {self.type} | Type: {self.type} |Rarity: {self.rarity}")
+
+# Create an instance of the Pokemon class
+my_pokemon = Pokemon("Pikachu", "Rare")
+
+# Call the method pokemon_info
+print(my_pokemon.pokemon_info())
