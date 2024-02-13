a28474e5adbb0bc77a0e6a12d8bcaec376f2586d
isnendyankp <isnendyankp@gmail.com>
2024-02-13 12:22:01 +0700

    cr8 dictionari data

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+dict = {
+    "name": "revou",
+    "age": 23,
+    "gender": [1, 2, 3, 4],
+    "family": {
+        "father" : "Budi",
+        "mother" : "Siti"
+    }   
+}
+
+print(dict["age"])
+# 23
+
+print(type(dict["family"]))
