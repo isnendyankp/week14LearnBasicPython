68f869d43ed34c5799493ce2b5cba981c85a2d0b
isnendyankp <isnendyankp@gmail.com>
2024-02-14 07:03:05 +0700

    cr8 sub class for inheritance

--- inheritance.py
+++ inheritance.py
@@ -7,3 +7,10 @@ class Animal:
         return "I am an animal"
     
 # derived class or sub class
+class Dog(Animal):
+    def speak(self):
+        return "woof!"
+    
+class Cat(Animal):
+    def speak(self):
+        return "meow!"
