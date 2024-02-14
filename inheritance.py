eb9b1bf3dfc1e90eb438fa88810be916a2cbb9eb
isnendyankp <isnendyankp@gmail.com>
2024-02-14 07:01:19 +0700

    cr8 base class for inheritance

---
+++ inheritance.py
@@ -0,0 +1,9 @@
+# base class or super class
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "I am an animal"
+    
+# derived class or sub class
